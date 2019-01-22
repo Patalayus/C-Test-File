9a3e1f8b0cf0a7dd36f050f4c8dd7cd9c9ad1b0d
Luke <lukecharles222@gmail.com>
2019-01-22 15:31:29 +0000

    while loop test
    a test where i increment the value of a variable each time i go through a while loop in c++

---
+++ test3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+        int a = 1;
+        while (a < 10){
+                cout << a << endl;
+                a++;
+        }
+}

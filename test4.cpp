256fac23e60b13d42eb820a3b142a454b55eae43
Luke <lukecharles222@gmail.com>
2019-01-22 16:43:34 +0000

    finished calculator
    so i used the previous while loop and added a basic calculator, which ive made in c++

--- test4.cpp
+++ test4.cpp
@@ -4,11 +4,33 @@ using namespace std;
 
 int main(){
         int a = 1;
-        while (a < 100){
+        double num1, num2;
+        int option1, total;
+        while (a < 101){
                 cout << "loading... " << a << "%" << endl;
                 a++;
-                if(a == 100){
+                if(a == 101){
                         cout << "finished" << endl;
                 }
+        }
+        cout << "Please enter a number\n";
+        cin >> num1;
+        cout << "Please enter another number\n";
+        cin >> num2;
+        cout << "Please select a function" << endl;
+        cout << "1. +\n2. -\n3. *\n4. /\n";
+        cin >> option1;
+        if(option1 == 1){
+                total = num1 + num2;
+                cout << "Your total is " << total << endl;
+        }else if(option1 == 2){
+                total = num1 - num2;
+                cout << "Your total is " << total << endl;
+        }else if(option1 == 3){
+                total = num1 * num2;
+                cout << "Your total is " << total << endl;
+        }else if(option1 == 4){
+                total = num1 / num2;
+                cout << "Your total is " << total << endl;
         }
 }
